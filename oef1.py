f4773031e383bda47d7077dbc50173e9b4377dcf
Lorik Qerkezi <Lorikik@hotmail.com>
2023-10-31 21:30:47 +0100

    json bestand toevoegen, main updaten

--- oef1.py
+++ oef1.py
@@ -1,9 +1,47 @@
 import platform
 import subprocess
+import json
+import os
+import sys
+
+SERVERS_BESTAND = "servers.json"
+
+servers = []
 
 keuze_modi = int(input("In welke modus wil je werken: (1) management modus, (2) check modus "))
 
-servers = []
+
+def laad_servers():
+    if os.path.exists(SERVERS_BESTAND):
+        try:
+            with open(SERVERS_BESTAND, "r") as f:
+                return json.load(f)
+        except json.decoder.JSONDecodeError:
+            print(f"Fout bij het laden van {SERVERS_BESTAND}. Het bestand is mogelijk leeg of ongeldig.")
+            return []
+    else:
+        return []
+
+def laad_servers():
+    if os.path.exists(SERVERS_BESTAND):
+        with open(SERVERS_BESTAND, "r") as f:
+            return json.load(f)
+    else:
+        return []
+
+def verwijder_server(server_naam):
+    global servers
+    for server in servers:
+        if server['naam'] == server_naam:
+            servers.remove(server)
+            sla_server_op(servers)
+            print(f"Server {server_naam} is verwijderd uit de lijst.")
+            return
+    print(f"Server {server_naam} niet gevonden in de lijst.")
+
+def sla_server_op(servers):
+    with open(SERVERS_BESTAND, "w") as f:
+        json.dump(servers, f, indent=2)
 
 def ping_serv(server):
     param = "-n" if platform.system().lower() == "windows" else "-c"
@@ -15,24 +53,30 @@ def ping_serv(server):
         status = "Onbereikbaar"
     return status
 
-def voeg_serv_toe(server):
-    servers.append(server)
-    print(f"Server {server} is toegevoegd aan de lijst")
+def voeg_serv_toe(server_naam):
+    servers.append({"naam": server_naam})
+    sla_server_op(servers)
+    print(f"Server {server_naam} is toegevoegd aan de lijst")
 
 while keuze_modi not in (1, 2):
     keuze_modi = int(input("Ongeldige keuze. Probeer opnieuw: (1) management modus, (2) check modus "))
 
+servers = laad_servers()
+
 if keuze_modi == 1:
     print("Management modus geactiveerd")
     keuze = int(input("Kies uit het menu: (1) Server toevoegen, (2) Server verwijderen, (3) Servers in lijst tonen "))
-    
+
     if keuze == 1:
         naam_toe = input("Geef naam van de server die je wil toevoegen: ")
         voeg_serv_toe(naam_toe)
     elif keuze == 2:
         naam_ver = input("Geef naam van de server die je wil verwijderen: ")
+        verwijder_server(naam_ver)
     elif keuze == 3:
-        print("Servers in lijst tonen")
+        print("Servers in lijst tonen:")
+        for server in servers:
+            print(server['naam'])
     else:
         print("Verkeerde keuze.")
 
